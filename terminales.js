c37b2c06dbfa452a41a3eb01b776021ca892741e
Wilmar <89958863+wgrojas@users.noreply.github.com>
2026-03-03 15:28:16 -0500

    first commit

--- terminales.js
+++ terminales.js
@@ -4,14 +4,14 @@
 // LISTA DE TERMINALES
 // =============================
 const IPS = {
-  "Terminal Giron canal 1": "181.48.219.230",
-  "Terminal Giron canal 2": "181.59.165.246",
-  "Bodega Giron canal 1": "186.80.90.57",
-  "Bodega Giron canal 2": "181.59.165.246",
+  // "Terminal Giron canal 1": "181.48.219.230",
+  // "Terminal Giron canal 2": "181.59.165.246",
+  // "Bodega Giron canal 1": "186.80.90.57",
+  // "Bodega Giron canal 2": "181.59.165.246",
+  // "Terminal Cucuta canal 1": "181.57.129.146",
+  // "Terminal Cucuta canal 2": "181.63.162.173",
   "AP Santacruz": "181.129.185.131:8070",
   "RB San Andres": "181.129.185.131:8071",
-  "Terminal Cucuta canal 1": "181.57.129.146",
-  "Terminal Cucuta canal 2": "181.63.162.173",
   "RouterBoard casa": "181.131.235.206:8090"
 
 };
